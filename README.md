fc2e3da650828f74cc69967f099816e599555c29
Daniel Lauzon <daniel.lauzon@gmail.com>
2018-09-28 05:12:48 -0400

    Working vt zone, need temporal scaling

--- README.md
+++ README.md
@@ -2,8 +2,18 @@
 
 ## Dev
 ```
-go run zone.go
+
+mkdir images
+rm images/zone-*png; go run zone.go
+docker run -v $(pwd)/images:/images --rm -it v4tech/imagemagick convert  -delay 1 /images/zone-*png -loop 0 /images/zone.gif
+#
+
+docker run -v $(pwd)/images:/images --rm -it v4tech/imagemagick identify /images/zone.gif
+open zone-*png
+
 ```
+
+
 ## References
 - CRC Code: `/archive/personal/CRC/CRC-daniel-2001-12-06.tar`
   - `Code/zone/zone.cc` and `makefile`
--- zone.go
+++ zone.go
@@ -12,26 +12,30 @@ import (
 
 func main() {
 	fmt.Println("Hello Zone")
-	var rect = image.Rect(0, 0, 640, 320)
+	// var rect = image.Rect(0, 0, 640, 320)
+	var rect = image.Rect(0, 0, 256, 256)
 	img := image.NewRGBA(rect)
-	render(img)
-	writePng("black.png", img)
+	frames := 60
+	for t := 0; t < frames; t++ {
+		t01 := 2.0 * math.Pi * (float64(t)/float64(frames) - .5)
+		render(img, t01, frames)
+		// render(img, 30.0)
+		writePng(fmt.Sprintf("./images/zone-%03d.png", t), img)
+
+	}
 }
 
-func render(img *image.RGBA) {
+func render(img *image.RGBA, t float64, frames int) {
 	rect := img.Bounds()
 	s := rect.Size()
 	cx := s.X / 2
 	cy := s.Y / 2
 
-	t := 15.0
-	period := 30.0 * 2.0
 	for j := rect.Min.Y; j < rect.Max.Y; j++ {
 		y := float64(j-cy) / float64(s.Y)
 		for i := rect.Min.X; i < rect.Max.X; i++ {
 			x := float64(i-cx) / float64(s.X)
-			phi := float64(period*x*t + float64(s.Y)*y*y) //* 2 * math.PI
-			// ima.bitmap[j][i] =  (pixel) (sin(phi)*126.+127.);
+			phi := (float64(frames*2)*x*t + float64(s.Y)*y*y) * 2 * math.Pi
 			c := uint8(math.Sin(phi)*126.0 + 127.0)
 			clr := color.RGBA{c, c, c, 255}
 			img.Set(i, j, clr)
