006e306bca607ade52d7e8a92b7cd696344212ff
Daniel Lauzon <daniel.lauzon@gmail.com>
2020-09-18 16:19:35 -0400

    Remove debug symbols in tinygo wasm output

--- nextjs-wasm-rust/public/wasm-go/build.sh
+++ nextjs-wasm-rust/public/wasm-go/build.sh
@@ -10,4 +10,4 @@ echo "-= Copying tinygo's wasm_exec.js"
 docker run -v $(pwd):/src  tinygo/tinygo:0.15.0 /bin/bash -c "cp -p /usr/local/tinygo/targets/wasm_exec.js /src/wasm_exec_tinygo.js"
 
 echo "-= Compiling tinygo.wasm with TinyGo"
-docker run --rm -v $(pwd):/src tinygo/tinygo:0.15.0 tinygo build -o /src/tinygo.wasm -target=wasm /src/main.go
+docker run --rm -v $(pwd):/src tinygo/tinygo:0.15.0 tinygo build -o /src/tinygo.wasm -target=wasm --no-debug /src/main.go
