993d92efccb95734424f5803fe3b3e970b8bb0e1
Daniel Lauzon <daniel.lauzon@gmail.com>
2020-09-18 21:10:30 -0400

    Cleanup docker --rm

--- nextjs-wasm-rust/public/wasm-go/build.sh
+++ nextjs-wasm-rust/public/wasm-go/build.sh
@@ -7,7 +7,7 @@ echo "-= Compiling main.wasm with Go"
 GOOS=js GOARCH=wasm go build -o main.wasm ./main.go
 
 echo "-= Copying tinygo's wasm_exec.js"
-docker run -v $(pwd):/src  tinygo/tinygo:0.15.0 /bin/bash -c "cp -p /usr/local/tinygo/targets/wasm_exec.js /src/wasm_exec_tinygo.js"
+docker run --rm -v $(pwd):/src  tinygo/tinygo:0.15.0 /bin/bash -c "cp -p /usr/local/tinygo/targets/wasm_exec.js /src/wasm_exec_tinygo.js"
 
 echo "-= Compiling tinygo.wasm with TinyGo"
 docker run --rm -v $(pwd):/src tinygo/tinygo:0.15.0 tinygo build -o /src/tinygo.wasm -target=wasm --no-debug /src/main.go
