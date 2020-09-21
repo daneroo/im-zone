d29e547a8f6795070f23a527a89f4c480c5a408e
Daniel Lauzon <daniel.lauzon@gmail.com>
2020-09-21 04:54:43 -0400

    Remove server rendered ref for now (non-static site)

--- nextjs-wasm-rust/pages/index.js
+++ nextjs-wasm-rust/pages/index.js
@@ -15,10 +15,13 @@ const Page = ({ router: { query } }) => {
         and reports the average render time.
         The Rust and Go implementations were compiled to WebAssembly.
         </p>
+        {/*
+        Need to deploy as non static... lerna time, so we can build on vercel/docker
         <Flex sx={{ width: '80%', margin: 'auto', alignItems: 'center', gap: 2 }}>
           <img src={`/api/zone/?bust=${1234}`} alt='Random' width='200' height='200' />
           <img src={`/api/zone/?bust=${4321}`} alt='Random' width='200' height='200' />
         </Flex>
+       */}
         <div>
           <p>Click on the Zone Plate or the <em>Play/Pause</em> button to animate.</p>
           <Box sx={{ color: 'secondary' }}>
