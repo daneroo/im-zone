4a2f9348d9cadbaf8dac6bc5ef135bd23e671ab8
Daniel Lauzon <daniel.lauzon@gmail.com>
2020-10-04 19:54:39 -0400

    Added Deck

---
+++ packages/deck/site.config.js
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: `@daneroo`,
+  title: `NextJS Conf '20`,
+  date: `October 27, 2020`,
+  author: {
+    url: `https://twitter.com/daneroo`,
+  },
+}
+
+export default siteConfig
