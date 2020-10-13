60bd869fddc2b17f42be88b9ad084ad876a867ca
Daniel Lauzon <daniel.lauzon@gmail.com>
2020-10-12 20:54:49 -0400

    Rebuild zoneplate-go

--- packages/zoneplate-go/dist/index.esm.js
+++ packages/zoneplate-go/dist/index.esm.js
@@ -561,7 +561,6 @@ async function importWasm () {
     return { renderGo }
   }
   try {
-    // console.log('imported Go is a', typeof Go)
     const go = new Go();
 
     const wasm = await instantiateB64(wasmBase64Bytes, go.importObject);
@@ -569,8 +568,9 @@ async function importWasm () {
 
     //  run the go instance
     go.run(instance);
+    console.log('Running Go instance');
 
-    console.log('imported Go WASM');
+    // get the exposed Go func from appropriate context
     if (typeof window !== 'undefined') {
       renderGo = window.DrawGo;
     } else if (typeof global !== 'undefined') {
